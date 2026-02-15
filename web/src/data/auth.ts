74b1a5a0209e39d3936fad079fe3019327a04918
KNR1997 <kethakaranasinghe@gmail.com>
2026-02-15 23:13:11 +0530

    feat: update userStore after login success

--- web/src/data/auth.ts
+++ web/src/data/auth.ts
@@ -1,9 +1,20 @@
-import { useMutation } from '@tanstack/vue-query'
+import { fetchMe } from './user'
+import { useUserStore } from '@/store'
 import { authClient } from './client/auth'
+import { setToken } from '@/utils/auth/token'
+import { useMutation } from '@tanstack/vue-query'
 import type { LoginInput, AuthResponse } from '@/types'
 
 export function useLogin() {
   return useMutation<AuthResponse, Error, LoginInput>({
     mutationFn: authClient.login,
+    onSuccess: async (response) => {
+      setToken(response.data)
+      // fetch user detail (username, email)
+      const me = await fetchMe()
+      // store details global state
+      const userStore = useUserStore()
+      userStore.setUserInfo(me.data)
+    },
   })
 }
