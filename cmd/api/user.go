bc0f5fda0373b0d43f9c2921dd405517d9457fe9
KNR1997 <kethakaranasinghe@gmail.com>
2026-02-16 19:01:42 +0530

    feat(handler): all users

--- cmd/api/user.go
+++ cmd/api/user.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/knr1997/assets-management-apiserver/internal/api/responses"
 	"github.com/knr1997/assets-management-apiserver/internal/store"
 )
@@ -65,7 +69,50 @@ func (app *application) updateUser(ctx context.Context, user *store.User) error 
 func (app *application) meDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	user := getUserFromContext(r)
 
-	resp := responses.NewUserResponse(user)
+	response := responses.NewUserResponse(user)
 
-	app.jsonResponse(w, http.StatusOK, resp)
+	app.jsonResponse(w, http.StatusOK, response)
+}
+
+func (app *application) getAllUserHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	users, err := app.store.Users.GetAll(ctx)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	response := responses.NewUsersResponse(users)
+
+	// app.jsonResponse(w, http.StatusOK, resp)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.internalServerError(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetByID(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundResponse(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
